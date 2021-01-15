55033c64027a2398cd8f05af0222c56b39891df5
florianpanchout <hi@florianpanchout.com>
2021-01-15 15:19:17 +0100

    Issue 5

--- src/views/search/results/suggestions/Suggestion.js
+++ src/views/search/results/suggestions/Suggestion.js
@@ -1,8 +1,20 @@
 import React, { useState, useEffect, useContext } from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 import SearchContext from 'utils/SearchContext'
 
+const flash = keyframes`
+  from,
+  2%,
+  4% {
+    opacity: 1;
+  }
+
+  1%,
+  3% {
+    opacity: 0;
+  }
+}`
 const Wrapper = styled.div`
   font-size: 2.6em;
   opacity: ${(props) => (props.mounted ? 1 : 0)};
@@ -11,11 +23,13 @@ const Wrapper = styled.div`
     transform 400ms ${(props) => props.index * 100}ms ease-in-out,
     text-shadow 300ms ease-out, font-size 200ms ease-out;
   cursor: pointer;
+  animation: ${flash} 35s ${(props) => props.index * 5 + 5}s infinite linear;
 
   &:hover {
     font-size: 3.2em;
   }
 `
+
 export default function Suggestion(props) {
   const { setSearch } = useContext(SearchContext)
 
