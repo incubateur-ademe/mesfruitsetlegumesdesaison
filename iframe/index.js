4f2d63cd554ffee9cccb086f5d120308bc095628
florianpanchout <hi@florianpanchout.com>
2021-01-21 10:11:10 +0100

    Iframe

--- iframe/index.js
+++ iframe/index.js
@@ -1,17 +1,17 @@
-//import { iframeResize } from 'iframe-resizer'
+import { iframeResize } from 'iframe-resizer'
 
 const script = document.getElementById('datagir-fruits-et-legumes'),
   distanceInitiale = encodeURIComponent(script.dataset.distanceInitiale),
   integratorUrl = encodeURIComponent(window.location.href.toString())
 
-const src = `https://mes-fruits-et-legumes-de-saisons.netlify.app/?distanceInitiale=${distanceInitiale}&iframe&integratorUrl=${integratorUrl}`
+const src = `https://mes-fruits-et-legumes-de-saisons.netlify.app/embed`
 
 const iframe = document.createElement('iframe')
 
 const iframeAttributes = {
   src,
   style:
-    'border: none; width: 100%; max-width: 750px; min-height: 750px; display: block; margin: 0 auto 10px;',
+    'border: none; width: 100%; min-height: 750px; display: block; margin: 0 auto 10px;',
   allowfullscreen: true,
   webkitallowfullscreen: true,
   mozallowfullscreen: true,
@@ -19,6 +19,6 @@ const iframeAttributes = {
 for (var key in iframeAttributes) {
   iframe.setAttribute(key, iframeAttributes[key])
 }
-//iframeResize({}, iframe)
+iframeResize({}, iframe)
 
 script.parentNode.insertBefore(iframe, script)
