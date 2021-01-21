b94460785232fb44ec7cab0bf6b06f39f8b2f626
florianpanchout <hi@florianpanchout.com>
2021-01-21 15:12:38 +0100

    Iframe Domain

--- iframe/index.js
+++ iframe/index.js
@@ -8,7 +8,7 @@ const search = script.dataset.search
 //const title = script.dataset.title
 const source = window.location.href.toString()
 
-const src = `${domain}/embed${search}&source=${source}`
+const src = `https://mes-fruits-et-legumes-de-saisons.netlify.app/embed${search}&source=${source}`
 
 const iframe = document.createElement('iframe')
 
