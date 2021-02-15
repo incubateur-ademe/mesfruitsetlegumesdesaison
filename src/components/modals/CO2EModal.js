2928f17102d54ff7c1d637d49c1686926bc35280
florianpanchout <hi@florianpanchout.com>
2021-02-15 20:10:36 +0100

    Update CO2 Modal

--- src/components/modals/CO2EModal.js
+++ src/components/modals/CO2EModal.js
@@ -11,26 +11,21 @@ export default function CO2EModal() {
   const { CO2E, setCO2E } = useContext(ModalContext)
 
   return (
-    <Modal open={CO2E} setOpen={setCO2E}>
+    <Modal open={CO2E} setOpen={setCO2E} textColor={'main'}>
       <Title>
-        L'équivalent CO<sub>2</sub> ou CO<sub>2</sub> équivalent (CO<sub>2</sub>
-        e)
+        L'équivalent CO<sub>2</sub> (CO<sub>2</sub>e)
       </Title>
       <Text>
-        Les différents GES anthropiques ont un impact plus ou moins important
-        sur le climat. Afin d’être comparés, les émissions des différents GES
-        peuvent être exprimés en CO<sub>2</sub>e (équivalent CO<sub>2</sub>).
-        Pour cela, le PRG (potentiel de réchauffement global) à 100 ans est
-        l'indicateur classique retenu dans la plupart des rapports et traités
-        internationaux. Par exemple, 1 kg de méthane (CH4) réchauffera autant
-        l’atmosphère que 28 à 30 kg de CO<sub>2</sub> au cours du siècle qui
-        suit leur émission.
+        Le dérèglement climatique actuel est une conséquence de nos émissions
+        importantes de gaz à effet de serre. Nous avons la chance de pouvoir
+        mesurer ces émissions avec un indice simple : les kilogrammes
+        d'équivalent CO<sub>2</sub> (kgCO<sub>2</sub>e).
       </Text>
       <Text>
-        <b>
-          Attention, cette valeur est donnée au kg de produit, et non à l'unité
-          !
-        </b>
+        Par exemple, 1 kg de méthane équivaut à 28 kg de CO<sub>2</sub>. Si la
+        fabrication d'un produit a émis 1 kg de méthane et 1 kg de CO
+        <sub>2</sub>, alors l'impact total de ce produit est de 29 kg
+        d'équivalent CO<sub>2</sub>.
       </Text>
     </Modal>
   )
