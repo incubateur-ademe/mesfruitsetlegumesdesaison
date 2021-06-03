fd7315581560f8a682552d41674199fc2a76b41c
Florian <hi@florianpanchout.com>
2021-06-03 09:44:02 +0200

    Iframe Id

--- iframe/index.js
+++ iframe/index.js
@@ -16,6 +16,7 @@ const iframe = document.createElement('iframe')
 
 const iframeAttributes = {
   src,
+  id: 'datagir-fruits-et-legumes-iframe',
   style: 'border: none; width: 100%; display: block; margin: 0 auto;',
   allowfullscreen: true,
   webkitallowfullscreen: true,
