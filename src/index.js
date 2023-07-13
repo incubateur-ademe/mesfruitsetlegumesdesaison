b9ca618eba92bedd460814ac8f4016741a756901
Florian <hi@florianpanchout.com>
2023-07-13 18:06:41 +0200

    force b-test on all but iframe

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import Cookies from 'js-cookie'
 
-let branch = Cookies.get('branch')
-if (!branch) {
-  branch = ['master', 'b-test'][Math.round(Math.random())]
-  console.log(branch)
-  Cookies.set('branch', branch, {
-    sameSite: 'none',
-    secure: true,
-  })
+if (!window.location.href.includes('iframe')) {
+  let branch = Cookies.get('nf_ab')
+  if (!branch) {
+    branch = 'b-test'
+    Cookies.set('nf_ab', branch, {
+      sameSite: 'none',
+      secure: true,
+    })
+    window.location.reload(true)
+  }
 }
-Cookies.set('nf_ab', branch, {
-  sameSite: 'none',
-  secure: true,
-})
 
 ReactDOM.render(<App />, document.getElementById('root'))
