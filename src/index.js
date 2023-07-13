f071c9a4d1cc5ce4dea3dba4486fc27acb4b7fed
Florian <hi@florianpanchout.com>
2023-07-13 09:31:04 +0200

    fix: manualy set nf_ab cookie with sameSite=None

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import Cookies from 'js-cookie'
+
+let branchRandomNumber = Cookies.get('branch_random_number')
+if (!branchRandomNumber) {
+  branchRandomNumber = Math.random()
+  Cookies.set('branch_random_number', branchRandomNumber, {
+    sameSite: 'None',
+    secure: true,
+  })
+}
+Cookies.set('nf_ab', branchRandomNumber, {
+  sameSite: 'None',
+  secure: true,
+})
 
 ReactDOM.render(<App />, document.getElementById('root'))
