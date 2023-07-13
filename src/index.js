ceae7727d9a2e4d2100fd62f032689fbc9d7b3e6
Florian <hi@florianpanchout.com>
2023-07-13 17:22:32 +0200

    feat: add branches name

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import Cookies from 'js-cookie'
 
-let branchRandomNumber = Cookies.get('branch_random_number')
-if (!branchRandomNumber) {
-  branchRandomNumber = Math.random()
-  Cookies.set('branch_random_number', branchRandomNumber, {
+let branch = Cookies.get('branch')
+if (!branch) {
+  branch = ['master', 'b-test'][Math.round(Math.random())]
+  console.log(branch)
+  Cookies.set('branch', branch, {
     sameSite: 'none',
     secure: true,
   })
 }
-Cookies.set('nf_ab', branchRandomNumber, {
+Cookies.set('nf_ab', branch, {
   sameSite: 'none',
   secure: true,
 })
